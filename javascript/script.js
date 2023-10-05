aa5163cf14e008dd69c02fc46218195500197c9a
vicky <vickyagarawal14300@gmail.com>
2023-10-05 12:46:33 +0530

    PORTFOLIO VICKY

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+// FOR HEADER 
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+// javascript for sidemenu
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+    sidemenu.style.right = "0";
+}
+
+function closemenu() {
+    sidemenu.style.right = "-200px";
+}
+
+
+//  FOR CONTACT FORM SEND EMAIL
